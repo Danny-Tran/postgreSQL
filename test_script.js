aea940f535e995c1abaf72c2bea50bbf4c27cd9c
Danny-Tran <truong_tran@live.com>
2019-05-21 22:23:03 +0000

    refractor added

--- test_script.js
+++ test_script.js
@@ -56,15 +56,16 @@ const queryType = process.argv.slice(2)[0];
 
 
 client.query("SELECT * FROM famous_people WHERE first_name=$1 OR last_name=$1", [queryType], (err, res) => {
-    if (err) {
-        console.log("ERR:", err)
-        return false
-        }
-        console.log(res.rows)
-        client.end()
-    })
+  if (err) {
+    console.log("ERR:", err)
+    return false
+  }
+  let j = 0
+  for (let index = 0; index < res.rows.length; index++) {
+    j++
+    console.log(`-${j}: ${res.rows[index].first_name} , ${res.rows[index].last_name} born ${res.rows[index].birthdate}`)
+  }
+    client.end()
+})
 
 
-// var nameSearch = (
-
-// )
